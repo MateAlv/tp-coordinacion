c10c575a1588d69d10242ea4f275e98a021818fb
Mate <mateoalvarez2012@gmail.com>
2026-04-20 18:22:25 -0300

    first message handler for gateway

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -7,12 +7,12 @@ class MessageHandler:
         pass
     
     def serialize_data_message(self, message):
-        [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
+        [client_id, fruit, amount] = message
+        return message_protocol.internal.serialize([client_id, fruit, amount])
 
-    def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+    def serialize_eof_message(self, client_id):
+        return message_protocol.internal.serialize([client_id])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
-        return fields
+        return (fields[0], fields[1])
