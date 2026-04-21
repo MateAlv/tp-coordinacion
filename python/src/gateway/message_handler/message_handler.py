015d606f1413af398543e4517c9273b1f145613e
Mate <mateoalvarez2012@gmail.com>
2026-04-20 21:55:33 -0300

    beginning EOF protocol

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -6,13 +6,15 @@ class MessageHandler:
 
     def __init__(self):
         self.client_id = str(uuid.uuid4())
+        self.message_count = 0
 
     def serialize_data_message(self, message):
         [fruit, amount] = message
+        self.message_count += 1
         return message_protocol.internal.serialize([self.client_id, fruit, amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([self.client_id])
+        return message_protocol.internal.serialize([self.client_id, self.message_count])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
